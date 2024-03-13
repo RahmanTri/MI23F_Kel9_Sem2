0c5d6058fcb1ad684179d21fc43af0942d3c9bb5
oliviamarsha202 <149671731+oliviamarsha202@users.noreply.github.com>
2024-03-13 21:36:24 +0700

    Update Tugas_LinkedList1.py

--- Tugas_LinkedList1.py
+++ Tugas_LinkedList1.py
@@ -26,3 +26,31 @@ def DisplayMenu():
           =============================================
           == Selamat Datang di E-Order Warung D4 MIE ==
           =============================================
+           Ice cream dan mie pedas tersedia disini!!
+          =============================================
+          
+Berikut merupakan menu kami :
+=============================""")
+    temp = head
+    while temp is not None:
+        print(temp.NamaMenu, "Rp.", temp.Harga)
+        temp = temp.next
+    print("""=============================
+Ketik menu yang akan dipesan, dan ketik "done" jika merasa cukup.""")
+    
+# Menambahkan menu dalam program
+addMenuItem("Mixue Ice Cream", 5000)
+addMenuItem("Boba Shake", 16000)
+addMenuItem("Mi Sundae", 14000)
+addMenuItem("Mie Ganas", 11000)
+addMenuItem("Creamy Mango Boba", 22000)
+
+# mendefinisikann variabel untuk daftar pesanan
+totalHarga = 0
+max_pesanan = 10
+pesanan = [""] * max_pesanan
+harga = [0] * max_pesanan
+Jml_Pesanan = 0
+
+# mulai menjalankan program
+DisplayMenu()
